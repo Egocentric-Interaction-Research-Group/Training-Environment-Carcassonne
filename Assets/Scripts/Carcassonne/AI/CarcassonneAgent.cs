39ef9d117a99cd6fc63f3a3d8a986a2a192f0590
kzeb61 <kasper.s.skott@gmail.com>
2021-12-06 21:50:01 +0100

    Added meeples to AI observations, and more.
    - Notice: different observation approaches may require different
      observation sizes. You are expected to adjust this accordingly.
    - Added a new observation approach, "Packed IDs". This is the smallest,
      and therefore fastest approach. It packs the tile ID, rotation,
      and meeple data into a single observation per tile.
    - Added additional information about each approach through the
      in-editor tooltips.

--- Assets/Scripts/Carcassonne/AI/CarcassonneAgent.cs
+++ Assets/Scripts/Carcassonne/AI/CarcassonneAgent.cs
@@ -6,7 +6,7 @@ using Unity.MLAgents.Sensors;
 using System;
 using Assets.Scripts.Carcassonne.AI;
 using static Carcassonne.Point;
-
+using System.Collections.Generic;
 
 /// <summary>
 /// The AI for the player. An AI user contains both a regular PlayerScript and this AI script to observe and take actions.
@@ -16,11 +16,19 @@ public class CarcassonneAgent : Agent
 {
     public enum ObservationApproach
     {
-        [Tooltip("Observation size: 917")]
-        [InspectorName("Tile Ids")] 
+        [InspectorName("Tile IDs")] 
+        [Tooltip("Observation size: 1817\nFor each tile, observe the tile ID and rotation as one "+
+                 "observation, and meeple data as another observation.")]
         TileIds,
-        
-        [Tooltip("Observation size: 1817")]
+
+        [InspectorName("Packed IDs")]
+        [Tooltip("Observation size: 917\nFor each tile, observe the tile ID, rotation, and "+
+                 "meeple data as one packed observation.")]
+        PackedIDs,
+
+        [Tooltip("Observation size: 1817\nFor each tile, pack all tile geographies explicitly, "+
+                 "into one observation (instead of using tile IDs), and then meeple data "+
+                 "as another observation.")]
         Packed
     }
 
@@ -52,6 +60,10 @@ public class CarcassonneAgent : Agent
 
             case ObservationApproach.Packed:
                 AddTileObservations = AddPackedTileObservations;
+                break;
+
+            case ObservationApproach.PackedIDs:
+                AddTileObservations = AddPackedTileIdObservations;
                 break;
 
                 // Note: There should only ever be one tile observations function in use, hence '=', and not '+='.
@@ -192,24 +204,111 @@ public class CarcassonneAgent : Agent
         }
     }
 
+    /// <summary>
+    /// This builds a dictionary of tile coordinates as keys, and 
+    /// meeple data (packed into a single int) as values.
+    /// 
+    /// Note that this is a shameless workaround for not being able to access the
+    /// meeple from the tile it occupies. If you could get the player id of the
+    /// meeple placed, via the tile, then this method wouldn't needed. 
+    /// Please make it happen, and end this suffering.
+    /// </summary>
+    /// <returns>A new dictionary of meeples that may be accessed using tile coordinates.</returns>
+    private Dictionary<Vector2Int, int> BuildMeepleMap()
+    {
+        List<Meeple> allMeeps = wrapper.state.meeples.All;
+        Dictionary<Vector2Int, int> mappedMeeps = new Dictionary<Vector2Int, int>(allMeeps.Count);
+        foreach (Meeple meep in allMeeps)
+        {
+            const int bitMask3 = 0x7; // 3-bit mask.
+            int meepleData = 0x0;
+            meepleData |= meep.playerId & bitMask3; // Insert 3-bit player id for meeple. Must be between 0-7.
+            meepleData |= ((int)meep.direction & bitMask3) << 3; // Insert 3-bit value for meeple direction.
+
+            mappedMeeps[new Vector2Int(meep.x, meep.z)] = meepleData;
+        }
+
+        return mappedMeeps;
+    }
+
     private void AddTileIdObservations(VectorSensor sensor)
     {
-        NewTile[,] playedTiles = wrapper.GetTiles();
-        foreach (NewTile t in playedTiles)
-        {
-            if (t == null)
-            {
-                sensor.AddObservation(0.0f);
-                continue;
-            }
+        Dictionary<Vector2Int, int> meepleMap = BuildMeepleMap();
+        NewTile[,] tiles = wrapper.GetTiles();
 
-            float obs = t.id + t.rotation * 100; // Note that tile ids must not exceed 99.
-            sensor.AddObservation(obs);  
+        for (int row = 0; row < tiles.GetLength(0); row++)
+        {
+            for (int col = 0; col < tiles.GetLength(1); col++)
+            {
+                NewTile tile = tiles[col, row];
+                if (tile == null)
+                {
+                    sensor.AddObservation(0.0f);
+                    sensor.AddObservation(-1.0f);
+                    continue;
+                }
+
+                float obs = tile.id + tile.rotation * 100; // Note that tile ids must not exceed 99.
+                sensor.AddObservation(obs);
+
+                // Add meeple data as a seperate observation.
+                int meepleData = 0x00;
+                if (meepleMap.TryGetValue(new Vector2Int(col, row), out meepleData))
+                {
+                    // Normalize by maximum, which is 6 bits set (63).
+                    float normalizedMeepleData = meepleData / (float)(0x3F);
+                    sensor.AddObservation(normalizedMeepleData);
+                }
+                else // If there was no meeple placed on this tile.
+                {
+                    sensor.AddObservation(-1.0f);
+                }
+            }
+        }
+    }
+
+    private void AddPackedTileIdObservations(VectorSensor sensor)
+    {
+        Dictionary<Vector2Int, int> meepleMap = BuildMeepleMap();
+        NewTile[,] tiles = wrapper.GetTiles();
+
+        for (int row = 0; row < tiles.GetLength(0); row++)
+        {
+            for (int col = 0; col < tiles.GetLength(1); col++)
+            {
+                NewTile tile = tiles[col, row];
+                if (tile == null)
+                {
+                    sensor.AddObservation(0.0f);
+                    continue;
+                }
+
+                int meepleData = 0x0;
+                if (!meepleMap.TryGetValue(new Vector2Int(col, row), out meepleData))
+                {
+                    meepleData = 0x1 << 6;
+                }
+
+                const int bitMask7 = 0x7F; // 7-bit mask.
+                const int bitMask6 = 0x3F; // 6-bit mask.
+                const int bitMask2 = 0x03; // 2-bit mask.
+
+                int packedData = 0x0;
+                packedData |= (tile.id & bitMask6);             // Tile id       = 6 bits
+                packedData |= (tile.rotation & bitMask2) << 6;  // Tile rotation = 2 bits
+                packedData |= (meepleData & bitMask7) << 8;     // Meeple data   = 7 bits
+
+                // If there is no meeple, the last 7 bits are '0b100 0000'.
+
+                float obs = packedData / (float)0x7FFF;         // Normalize using maximum (15 bits).
+                sensor.AddObservation(obs);
+            }
         }
     }
 
     private void AddPackedTileObservations(VectorSensor sensor)
     {
+        Dictionary<Vector2Int, int> meepleMap = BuildMeepleMap();
         NewTile[,] tiles = wrapper.GetTiles();
 
         for (int row = 0; row < tiles.GetLength(0); row++)
@@ -222,16 +321,12 @@ public class CarcassonneAgent : Agent
                 
                 if (tile == null)
                 {
-                    tileData = unchecked((int)0xFFFFFFFF); // No data = -1.
-                    sensor.AddObservation(tileData);
-                    sensor.AddObservation(meepleData);
+                    sensor.AddObservation(-1.0f); // Tile
+                    sensor.AddObservation(-1.0f); // Meeple
                     continue;
                 }
 
                 const int bitMask4  = 0xF; // 4-bit mask.
-                const int bitMask3  = 0x7; // 3-bit mask.
-                int meeplePlayerId  = -1;   // TODO: implement when there is a way to access the meeple from a tile.
-                int meepleDirection = 0;   // TODO: see above.
 
                 tileData |= ((int)tile.Center & bitMask4);
                 tileData |= (((int)tile.East  & bitMask4) << 4);
@@ -239,20 +334,19 @@ public class CarcassonneAgent : Agent
                 tileData |= (((int)tile.West  & bitMask4) << 12);
                 tileData |= (((int)tile.South & bitMask4) << 16);
 
-                if (meeplePlayerId >= 0) // If there IS a meeple placed on this tile.
+                // Load the meeple data into "meepleData" if there is a meeple on this tile.
+                if (!meepleMap.TryGetValue(new Vector2Int(col, row), out meepleData))
                 {
-                    meepleData |= (meeplePlayerId        & bitMask3) << 20; // Insert 3-bit player id for meeple. Should be between 0-7.
-                    meepleData |= ((meepleDirection + 1) & bitMask3) << 23; // Insert 3-bit value for meeple direction.
+                    // If there was no meeple, set only the 7th bit (bit 6),
+                    // in order to indicate 'no meeple'.
+                    meepleData = 0x1 << 6;
                 }
-
-                // In total, 26 of 32 bits are used to store geography data and meeple placement,
-                // and the id of its owner.
 
                 // Normalize by maximum, which is 20 bits set (1,048,575).
                 float normalizedTileData = tileData / (float)(0xFFFFF);
 
-                // Normalize by maximum, which is 6 bits set (63).
-                float normalizedMeepleData = meepleData / (float)(0x3F);
+                // Normalize by maximum, which is 7 bits set (127).
+                float normalizedMeepleData = meepleData / (float)0x7F;
 
                 sensor.AddObservation(normalizedTileData);
                 sensor.AddObservation(normalizedMeepleData);
